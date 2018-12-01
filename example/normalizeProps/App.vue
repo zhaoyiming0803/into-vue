39a6c08ed2b8976ab6afbc250a4d8feb78f2579b
zymseo <1047832475@qq.com>
2018-12-01 22:44:53 +0800

    update

---
+++ example/normalizeProps/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="root-container">
+    <hello :person="person"></hello>
+  </div>
+</template>
+
+<script scoped type="ecmascript-6">
+  import hello from './components/hello';
+  export default {
+    data () {
+      return {
+        person: {
+          name: 'zhaoyiming',
+          age: 18
+        }
+      }
+    },
+
+    components: {
+      hello
+    }
+  }
+</script>
---
+++ example/normalizeProps/hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ person.name }}
+    {{ person.age }}
+  </div>
+</template>
+
+<script scoped type="ecmascript-6">
+  export default {
+    props: {
+      person: {
+        type: Object
+      }
+    },
+    
+    data () {
+      return {
+        
+      }
+    }
+  }
+</script>
