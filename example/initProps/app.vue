bd9a59594404f2b1a55e732772f46a5088e28caa
zymseo <1047832475@qq.com>
2019-06-09 21:02:58 +0800

    doc: initProps

---
+++ example/initProps/app.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="app-container">
+    <hello :message="message" :content="content" :test="test"></hello>
+    <hello :message="message" :content="content"></hello>
+    <button @click="switchHello">切换hello组件</button>
+  </div>
+</template>
+
+<script>
+  import Hello from './components/hello';
+
+  export default {
+    data () {
+      return {
+        isShowTest: true,
+        message: 'hello world',
+        content: {},
+        formData: {},
+        test: 'test'
+      }
+    },
+
+    components: {
+      Hello
+    },
+
+    mounted () {
+      console.log(this.$parent);
+    },
+
+    methods: {
+      switchHello () {
+        this.isShowTest = false;
+        this.$nextTick(() => {
+          this.test = undefined;
+        });
+      }
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
